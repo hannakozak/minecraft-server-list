d7717d116ea1321ba737fa3f77a890efe1f75fc1
hanna <hannawkozak@gmail.com>
2025-01-25 23:14:00 +0000

    style: add colors definintion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	darkMode: 'selector',
+	theme: {
+		extend: {
+			colors: {
+				'green-light': '#BBF7D0',
+				'green-medium': '#37de75',
+				'green-dark': '#0a3e1e',
+				'gray-light': '#EBEBEB',
+				'gray-medium': '#64748B',
+				'gray-dark': '#282828',
+				'text-white': '#ffffff',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
